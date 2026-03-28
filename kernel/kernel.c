1725a2a652d10051754198945ec96b54ab16469a
massah <arafeh.massah2007@gmail.com>
2026-03-28 18:11:03 -0400

    strengthen error handling, changed input handling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,7 +11,6 @@
 #include "../include/net.h"
 
 #define NUM_WORKERS 3 // TODO: change as needed
-#define NUM_ROUNDS 5  // TODO: change as needed
 #define NUM_PROCESSES (1 + NUM_WORKERS)
 
 typedef struct
@@ -24,6 +23,54 @@ typedef struct
 
 static ProcessEntry processes[NUM_PROCESSES];
 static int nprocesses = 0;
+
+static void fork_worker(uint32_t worker_id)
+{
+      int k2w[2];
+      int w2k[2];
+
+      if (pipe(k2w) < 0 || pipe(w2k) < 0)
+      {
+            perror("pipe");
+            exit(1);
+      }
+
+      pid_t pid = fork();
+
+      if (pid < 0)
+      {
+            perror("fork");
+            exit(1);
+      }
+
+      if (pid == 0) // child process
+      {
+            char read_fd_str[16];
+            char write_fd_str[16];
+            char worker_id_str[16];
+
+            close(k2w[1]);
+            close(w2k[0]);
+
+            snprintf(read_fd_str, sizeof(read_fd_str), "%d", k2w[0]);
+            snprintf(write_fd_str, sizeof(write_fd_str), "%d", w2k[1]);
+            snprintf(worker_id_str, sizeof(worker_id_str), "%u", worker_id);
+
+            execl("./workers", "worker", read_fd_str, write_fd_str, worker_id_str, (char *)NULL);
+            perror("execl");
+            exit(1);
+      }
+
+      // parent process
+      close(k2w[0]);
+      close(w2k[1]);
+
+      processes[nprocesses].process_id = worker_id;
+      processes[nprocesses].read_fd = w2k[0];
+      processes[nprocesses].write_fd = k2w[1];
+      processes[nprocesses].pid = pid;
+      nprocesses++;
+}
 
 static void fork_param_server(void)
 {
@@ -49,23 +96,36 @@ static void fork_param_server(void)
             close(k2p[1]);
             close(p2k[0]);
 
-            if (dup2(k2p[0], STDIN_FILENO) == -1)
+            char kernel_read_fd_str[16];
+            char kernel_write_fd_str[16];
+            char num_workers_str[16];
+
+            char worker_read_fd_str[NUM_WORKERS][16];
+            char worker_write_fd_str[NUM_WORKERS][16];
+            char *argv[5 + (NUM_WORKERS * 2)];
+            int arg_idx = 0;
+
+            snprintf(kernel_read_fd_str, sizeof(kernel_read_fd_str), "%d", k2p[0]);
+            snprintf(kernel_write_fd_str, sizeof(kernel_write_fd_str), "%d", p2k[1]);
+            snprintf(num_workers_str, sizeof(num_workers_str), "%d", NUM_WORKERS);
+
+            argv[arg_idx++] = (char *)"param_server";
+            argv[arg_idx++] = kernel_read_fd_str;
+            argv[arg_idx++] = kernel_write_fd_str;
+            argv[arg_idx++] = num_workers_str;
+
+            for (int i = 0; i < NUM_WORKERS; i++)
             {
-                  perror("dup2");
-                  exit(1);
-            };
+                  snprintf(worker_read_fd_str[i], sizeof(worker_read_fd_str[i]), "%d", processes[i].read_fd);
+                  snprintf(worker_write_fd_str[i], sizeof(worker_write_fd_str[i]), "%d", processes[i].write_fd);
+                  argv[arg_idx++] = worker_read_fd_str[i];
+                  argv[arg_idx++] = worker_write_fd_str[i];
+            }
 
-            if (dup2(p2k[1], STDOUT_FILENO) == -1)
-            {
-                  perror("dup2");
-                  exit(1);
-            };
+            argv[arg_idx] = NULL;
 
-            close(k2p[0]);
-            close(p2k[1]);
-
-            execl("./workers", "param_server", NULL);
-            perror("execl");
+            execv("./workers", argv);
+            perror("execv");
             exit(1);
       }
 
@@ -73,77 +133,22 @@ static void fork_param_server(void)
       close(k2p[0]);
       close(p2k[1]);
 
-      processes[nprocesses].process_id = SERVICE_SCHEDULER;
+      processes[nprocesses].process_id = SERVICE_MODEL;
       processes[nprocesses].read_fd = p2k[0];
       processes[nprocesses].write_fd = k2p[1];
       processes[nprocesses].pid = pid;
       nprocesses++;
 }
 
-static void fork_worker(uint32_t worker_id)
-{
-      int k2w[2];
-      int w2k[2];
-
-      if (pipe(k2w) < 0 || pipe(w2k) < 0)
-      {
-            perror("pipe");
-            exit(1);
-      }
-
-      pid_t pid = fork();
-
-      if (pid < 0)
-      {
-            perror("fork");
-            exit(1);
-      }
-
-      if (pid == 0) // child process
-      {
-            close(k2w[1]);
-            close(w2k[0]);
-
-            if (dup2(k2w[0], STDIN_FILENO) == -1)
-            {
-                  perror("dup2");
-                  exit(1);
-            };
-
-            if (dup2(w2k[1], STDOUT_FILENO) == -1)
-            {
-                  perror("dup2");
-                  exit(1);
-            }
-
-            close(k2w[0]);
-            close(w2k[1]);
-
-            execl("./workers", "worker", NULL);
-            perror("execl");
-            exit(1);
-      }
-
-      // parent process
-      close(k2w[0]);
-      close(w2k[1]);
-
-      processes[nprocesses].process_id = worker_id;
-      processes[nprocesses].read_fd = w2k[0];
-      processes[nprocesses].write_fd = k2w[1];
-      processes[nprocesses].pid = pid;
-      nprocesses++;
-}
-
-static void register(void)
+static void register_processes(void)
 {
       for (int i = 0; i < nprocesses; i++)
       {
             Message msg;
-            if (recv_message(processes[i].readfd, &msg) < 0)
+            if (recv_message(processes[i].read_fd, &msg) < 0)
             {
-                  fprintf(stderr, "[kernel %u] ERROR: failed to read OP_REGISTER\n", processes[i].process_id)
-                      exit(1);
+                  fprintf(stderr, "[kernel %u] ERROR: failed to read OP_REGISTER\n", processes[i].process_id);
+                  exit(1);
             }
             if (msg.header.opcode != OP_REGISTER)
             {
@@ -152,10 +157,16 @@ static void register(void)
             }
 
             RegisterAckPayload ack;
-            ack.servicetype = msg.header.src_id;
+            if (msg.header.payload_size >= sizeof(RegisterPayload)) // precaution if payload is too small, just set servicetype to 0
+            {
+                  RegisterPayload *reg = (RegisterPayload *)msg.payload;
+                  ack.servicetype = reg->servicetype;
+            }
+            else
+                  ack.servicetype = 0;
             ack.assigned_id = processes[i].process_id;
 
-            if (send_message(processes[i].write_fd, processes[i].process_id, SERVICE_KERNEL, OP_REGISTER_ACK, &ack, sizeof(ack)) < 0)
+            if (send_message(processes[i].write_fd, SERVICE_KERNEL, processes[i].process_id, OP_REGISTER_ACK, &ack, sizeof(ack)) < 0)
             {
                   fprintf(stderr, "[kernel %u] ERROR: failed to send OP_REGISTER_ACK\n", processes[i].process_id);
                   exit(1);
@@ -164,100 +175,34 @@ static void register(void)
       }
 }
 
-static int find_write_fd(uint32_t process_id)
+static void wait_for_children(void)
 {
-      for (int i = 0; i < nprocesses; i++)
-      {
-            if (processes[i].process_id == process_id)
-                  return processes[i].write_fd;
-      }
-      return -1;
-}
-
-static void run_rounds(void)
-{
-      for (int round = 0; round < NUM_ROUNDS; round++)
-      {
-            fprintf(stderr, "[kernel] starting round %d\n" round);
-
-            RoundStartPayload rs;
-            rs.round_id = round;
-
-            for (int i = 1; i < nprocesses; i++) // start at 1 to skip param_server
-            {
-                  if (send_message(processes[i].write_fd, processes[i].process_id, SERVICE_KERNEL, OP_ROUND_START, &rs, sizeof(rs)) < 0)
-                  {
-                        fprintf(stderr, "[kernel %u] ERROR: failed to send OP_ROUND_START\n" processes[i].process_id);
-                        exit(1);
-                  }
-            }
-
-            // collect grads and send weights to param server
-            for (int i = 1; i <= NUM_WORKERS; i++)
-            {
-                  Message msg;
-                  if (recv_message(processes[i].write_fd, &msg) < 0)
-                  {
-                        fprintf(stderr, "[kernel %u] ERROR: failed to read OP_SUBMIT_GRADIENT\n", processes[i].process_id);
-                        continue;
-                  }
-
-                  msg.header.dest_id = processes[0].process_id;
-                  if (send_message(processes[i].write_fd, processes[i].processed_id, SERVICE_KERNEL, msg_header.opcode, msg.payload, msg.header.payload_size) < 0)
-                  {
-                        fprintf(stderr, "[kernel %u] ERROR: failed to forward gradient to param server\n", processes[i].process_id);
-                        break;
-                  }
-            }
-
-            Message resp;
-            if (recv_message(processes[0].write_fd, &resp) < 0)
-            {
-                  fprintf(stderr, "[kernel] ERROR: failed to receive updated weights from param server\n");
-                  continue;
-            }
-
-            for (int i = 1; i < nprocesses, i++)
-            {
-                  if (send_message(processes[i].write_fd, processes[i].process_id, SERVICE_KERNEL, resp.header.opcode, resp.payload, resp.header.payload_size) < 0)
-                  {
-                        fprintf(stderr, "[kernel %u] ERROR: failed to send updated weights to worker\n", processes[i].process_id);
-                        continue;
-                  }
-            }
-      }
-}
-
-static void terminate(void)
-{
-      for (int i = 0; i < nprocesses; i++)
-      {
-            if (send_message(processes[i].write_fd, processes[i].process_id, SERVICE_KERNEL, OP_TERMINATE, NULL, 0) < 0)
-            {
-                  fprintf(stderr, "[kernel %u] ERROR: failed to send OP_TERMINATE\n", processes[i].process_id);
-                  exit(1);
-            }
-            close(processes[i].write_fd);
-      }
-
       for (int i = 0; i < nprocesses; i++)
       {
             int status;
             waitpid(processes[i].pid, &status, 0);
-            fprintf(stderr, "[kernel] pid %d exited with status%d\n", processes[i].pid, WEXITSTATUS(status));
+            fprintf(stderr, "[kernel] pid %d exited with status %d\n", processes[i].pid, WEXITSTATUS(status));
+      }
+}
+
+static void close_pipes(void)
+{
+      for (int i = 0; i < nprocesses; i++)
+      {
+            close(processes[i].write_fd);
             close(processes[i].read_fd);
       }
 }
 
 int main(void)
 {
-      fork_param_server();
-      for (uint32_t i = 0; i < NUM_WORKERS; i++)
+      for (uint32_t i = 1; i <= NUM_WORKERS; i++)
             fork_worker(i);
+      fork_param_server();
 
-      register();
-      run_rounds();
-      terminate();
+      register_processes();
+      wait_for_children();
+      close_pipes();
 
       fprintf(stderr, "[kernel] all processes terminated, exiting\n");
       return 0;
